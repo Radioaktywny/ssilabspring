8b032fe4aa66a50de5d9901283e7b3ee3878c8a2
Marcin <markowitek@gmail.com>
2016-12-12 20:21:25 +0100

    create simple get and post to test api functionality , everythings works properly

--- src/main/java/com/api/StudentController.java
+++ src/main/java/com/api/StudentController.java
@@ -1,8 +1,18 @@
 package com.api;
 
+import com.domain.Student;
 import com.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
 
 /**
  * Created by Marcin on 12.12.2016.
@@ -14,5 +24,22 @@ public class StudentController {
     private StudentService studentService;
 
 
+    @RequestMapping(value = "/api/save_student",
+            method = RequestMethod.POST,
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Student> createAccount(@RequestBody Student student) {
+        studentService.save(student);
+        return new ResponseEntity<>(student, HttpStatus.CREATED);
+    }
+
+
+    @RequestMapping(value = "/api/get_students",
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<Student>> getStudents() {
+        List<Student> students = studentService.findAll();
+        return new ResponseEntity<>(students, HttpStatus.OK);
+    }
 
 }
